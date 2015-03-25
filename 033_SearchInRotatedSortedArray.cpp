d71ae97541e9e859de7ac8900c46eb5b70243581
liuxiangbin <mycodekingdom@gmail.com>
2015-03-25 22:12:20 +0800

    Problem 033: Search In Rotated Sorted Array

---
+++ 033_SearchInRotatedSortedArray.cpp
@@ -0,0 +1,22 @@
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+*/
+
+class Solution {
+public:
+    int search(int A[], int n, int target) {
+        int result = -1;
+        for(int i=0;i<n;++i){
+            if(A[i]==target)
+                result = i;
+        }
+        
+        return result;
+    }
+};

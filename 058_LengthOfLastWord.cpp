b48f8c85288221ffc46eea971260ab81c081baa9
xb_liu <mycodekingdom@gmail.com>
2015-03-29 23:03:53 +0800

    Problem 058: Length Of Last Word

---
+++ 058_LengthOfLastWord.cpp
@@ -0,0 +1,30 @@
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example,
+Given s = "Hello World",
+return 5. 
+*/
+
+class Solution {
+public:
+    int lengthOfLastWord(const char *s) {
+        int len = strlen(s);
+        
+        while(len>0 && s[len-1]==' ')
+            --len;
+            
+        if(len==0)
+            return 0;
+            
+        int res = 0;
+        while(res<len && isalpha(s[len-1-res]))
+            ++res;
+            
+        return res;
+    }
+};
